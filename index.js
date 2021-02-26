e0309d594a393b16a0b621ab01e5147b995c9c82
MING <TheSky@ming-shihs-mbp.lan>
2021-02-26 18:00:25 +0800

    commit

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const SideBar = () => {
     const burger = document.querySelector('.burger');
     const sidebar = document.querySelector('.sidebar');
+    
+    function slideaway() {
+        sidebar.style.transition = ''
+    };
 
     burger.addEventListener('click', () => {
         sidebar.classList.toggle('sidebar-active');
+
+        
+        // Sidebar animation
+        if (sidebar.style.transition) {
+            setTimeout(slideaway, 10000);
+            console.log('no transition');
+        } else {
+            sidebar.style.transition = 'transform 0.2s ease-out';
+            console.log('has transition');
+        }
+
+        // burger animation
+        burger.classList.toggle('toggle');
+
     });
 }
 
