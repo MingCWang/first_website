91616f4c6bacde70d3ccf5862c8e37f4c4760459
MING <TheSky@ming-shihs-mbp.lan>
2021-02-28 17:31:32 +0800

    commit

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
 const SideBar = () => {
     const burger = document.querySelector('.burger');
     const sidebar = document.querySelector('.sidebar');
+    const shade = document.querySelector('.shade');
     
+
     function slideaway() {
         sidebar.style.transition = ''
     };
 
+
+    
+
     burger.addEventListener('click', () => {
+        
         sidebar.classList.toggle('sidebar-active');
 
-        
         // Sidebar animation
         if (sidebar.style.transition) {
-            setTimeout(slideaway, 10000);
-            console.log('no transition');
+            setTimeout(slideaway, 500);
         } else {
             sidebar.style.transition = 'transform 0.2s ease-out';
-            console.log('has transition');
         }
 
         // burger animation
         burger.classList.toggle('toggle');
 
+        // Shade
+        shade.classList.toggle('shade-show');
     });
 }
 
