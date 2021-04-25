a02f50cf3b5c9a5898aa4c568e0df006e808f779
MingCWang <minstone@gmail.com>
2021-04-25 18:03:49 +0800

    added + and - tranition sign

--- JS/dropdown.js
+++ JS/dropdown.js
@@ -2,11 +2,14 @@ function dropdown() {
     const box = Array.apply(null, document.querySelectorAll('.box'));
     const contactdropdown = document.querySelector('.contactdropdown');
     const callBox = document.querySelector('.call-box');
-
-
+    const sendmailbox = document.querySelector('.sendmail-box');
+    const open = document.querySelector('.open');
+    const close = document.querySelector('.close');
     box.forEach(item => {
         item.addEventListener('click' , () => {
             item.classList.toggle('box-active');
+            open.classList.toggle('open-active');
+            close.classList.toggle('close-active');
         });
     });
 
@@ -16,6 +19,13 @@ function dropdown() {
         contactdropdown.classList.toggle('contactdropdown-active');
 
     });
+
+    sendmailbox.addEventListener('click', () => {
+
+
+
+    });
+
 
 
 
