6c434a69f551e52ddd5c78047a89b4fbb59b2fb5
MingCWang <minstone@gmail.com>
2021-06-12 00:39:28 +0800

    finished message us dropdown

--- JS/dropdown.js
+++ JS/dropdown.js
@@ -1,31 +1,34 @@
 function dropdown() {
     const box = Array.apply(null, document.querySelectorAll('.box'));
-    const contactdropdown = document.querySelector('.contactdropdown');
+    const contactdropdowncall = document.querySelector('.contactdropdowncall');
+    const contactdropdownmessage = document.querySelector('.contactdropdownmessage');
     const callBox = document.querySelector('.call-box');
     const sendmailbox = document.querySelector('.sendmail-box');
+    const messagebox = document.querySelector('.message-box');
     const open = document.querySelector('.open');
     const close = document.querySelector('.close');
     box.forEach(item => {
         item.addEventListener('click' , () => {
             item.classList.toggle('box-active');
-            open.classList.toggle('open-active');
-            close.classList.toggle('close-active');
         });
     });
 
 
     callBox.addEventListener('click' , () => {
-       
-        contactdropdown.classList.toggle('contactdropdown-active');
+        open.classList.toggle('open-active');
+        close.classList.toggle('close-active');
+        contactdropdowncall.classList.toggle('contactdropdowncall-active');
 
     });
 
     sendmailbox.addEventListener('click', () => {
 
-
-
     });
 
+    messagebox.addEventListener('click', () => {
+
+        contactdropdownmessage.classList.toggle('contactdropdownmessage-active');
+    });
 
 
 
